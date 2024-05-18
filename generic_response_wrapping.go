f32edfeef1a3ecc7543611a06373a192b9160461
joe3838298 <joe3838298@gmail.com>
2024-05-18 06:31:06 +0000

    chore: format golang code files

--- generic_response_wrapping.go
+++ generic_response_wrapping.go
@@ -8,39 +8,39 @@ import (
 )
 
 type NaiveResponse struct {
-    Data interface{} `json:"data"`
+	Data interface{} `json:"data"`
 }
 
 type Person struct {
-    Name string `json:"name"`
-    Age int `json:"age"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 // NOTE: NaiveResponse and Response both implement the same json schema
 func send(person Person) []byte {
-    payload, _ := json.Marshal(response.Response[Person]{
+	payload, _ := json.Marshal(response.Response[Person]{
 		Data: person,
 	})
-    return payload
+	return payload
 }
 
 func receiveWithAssertion(payload []byte) {
-    var response NaiveResponse
-	
-    err := json.Unmarshal(payload, &response)
+	var response NaiveResponse
+
+	err := json.Unmarshal(payload, &response)
 	if err != nil {
 		panic(err)
 	}
 
 	// NOTE: it's the pitfall when using assertion
-	// 
-    // person, ok := response.Data.(Person)
+	//
+	// person, ok := response.Data.(Person)
 	person, ok := response.Data.(map[string]interface{})
-    if !ok {
-        panic(fmt.Sprintf("invalid data: %s", response))
-    }
+	if !ok {
+		panic(fmt.Sprintf("invalid data: %s", response))
+	}
 
-    fmt.Printf("data: %+v\n", person)
+	fmt.Printf("data: %+v\n", person)
 }
 
 func receive(payload []byte) {
@@ -57,12 +57,12 @@ func receive(payload []byte) {
 func main() {
 	person := Person{
 		Name: "John",
-		Age: 20,
+		Age:  20,
 	}
 
 	payload := send(person)
 
-	// show two receive function presenting the same output 
+	// show two receive function presenting the same output
 	receiveWithAssertion(payload)
 	receive(payload)
 }
